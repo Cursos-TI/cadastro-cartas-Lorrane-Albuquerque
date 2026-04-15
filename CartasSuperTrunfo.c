ca55796a3cfe743f2cf342599e6ea475e59f9bc8
Lorrane-Albuquerque <lorranealbuquerque56@gmail.com>
2026-04-15 23:17:08 +0000

    Nivel Novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,59 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+char Carta[5]
+char Estado;
+char Codigo[04];
+char Cidade[20];
+int População;
+float Area;
+int PIB;
+int PontosTuristicos;
+
 
   // Área para entrada de dados
+printf ("Digite numero da carta de 1 a 2:\n")
+scanf ("%s",Carta);
 
-  // Área para exibição dos dados da cidade
+printf ("Digite Uma letra de 'A' a 'H' para o estado:\n")
+scanf ("%c",&Estado);
+
+printf ("Digite um codigo de 01 a 04:\n")
+scanf ("%s",Codigo);
+
+printf ("Digite o nome da Cidade:\n")
+scanf ("%s",Cidade);
+
+printf ("Digite o número da população:\n")
+scanf ("%i",&População);
+
+printf ("Digite A área da cidade em quilômetros quadrados:\n")
+scanf ("%f",&Area);
+
+printf ("Digite O Produto Interno Bruto da cidades:\n")
+scanf ("%i",&PIB);
+
+printf ("Digite A quantidade de pontos turísticos na cidade:\n")
+scanf ("%i",PontosTuristicos);
+
+// Área para exibição dos dados da cidade
+
+printf("Carta: %s\n", Carta);
+
+printf("Estado: %c\n", Estado);
+
+printf("Codigo: %c,%s\n", Estado, Codigo);
+
+printf("Cidade: %c\n", Cidade);
+
+printf("População: %i\n", &População);
+
+printf("Area: %f\n", &Area);
+
+printf("PIB: %i\n", &PIB);
+
+printf("Pontos Turisticos: %i, &PontoTurusticos")
+
 
 return 0;
 } 
